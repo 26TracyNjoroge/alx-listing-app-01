7c2659429a2dcec14c36c24eef04c920f6f07e93
Tracy Njoroge <tracywangari997@gmail.com>
2025-06-16 23:45:34 +0300

    Update the detail page

--- components/property/PropertyDetail.tsx
+++ components/property/PropertyDetail.tsx
@@ -18,7 +18,7 @@ const PropertyDetail: React.FC<{ property: PropertyProps }> = ({ property }) => 
       {/* Description */}
       <div className="mt-4">
         <h2 className="text-2xl font-semibold">Description</h2>
-        <p>{property.description}</p>
+        <p>{property.name}</p>
       </div>
 
       {/* Amenities */}
--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,6 +1,7 @@
 import { PROPERTYLISTINGSAMPLE } from "@/constants/index";
 import { useRouter } from "next/router";
 import PropertyDetail from "@/components/property/PropertyDetail";
+import BookingSection from "@/components/property/BookingSection";
 
 export default function PropertyPage() {
   const router = useRouter();
@@ -10,8 +11,20 @@ export default function PropertyPage() {
   if (!property) return <p>Property not found</p>;
 
   return (
-    <div>
-      <PropertyDetail property={property} />
+    <div className="container mx-auto p-6">
+      <div className="">
+        {/* Main content - Property details and reviews */}
+        <div className="">
+          <PropertyDetail property={property} />
+        </div>
+        
+        {/* Sidebar - Booking section */}
+        <div className="">
+          <div className="w-96">
+            <BookingSection price={property.price} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
